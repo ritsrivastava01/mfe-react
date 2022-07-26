31e405ab30ff9c49ecdb8ac6f01ab562e2ca97bd
Ritz <ritsrivastava@gmail.com>
2022-07-26 12:03:39 +0200

    NA

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Hi Ritesh</h1>
+      <h1>Hi Ritesh!!!!</h1>
       <hr></hr>
       <MarketingApp></MarketingApp>
     </div>
